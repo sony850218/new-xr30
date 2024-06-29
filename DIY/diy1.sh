36d749a79c67dff6a63a1386e408342bcfa5b859
sony850218 <161965688+sony850218@users.noreply.github.com>
2024-06-30 00:40:46 +0800

    Update diy1.sh

--- DIY/diy1.sh
+++ DIY/diy1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #=================================================
-# MZwrt script
+# ImmortalWrt script
 #================================================= 
 
 
 ##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/mzwrt/extra_ipk" >> feeds.conf.default
+echo -e "\nsrc-git extraipk https://github.com/sony850218/ipk" >> feeds.conf.default
