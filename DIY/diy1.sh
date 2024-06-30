cda5a77c9ab9c596f19c6d45e94b459cc9720813
sony850218 <161965688+sony850218@users.noreply.github.com>
2024-06-30 11:08:58 +0800

    Delete DIY directory

--- DIY/diy1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#=================================================
-# ImmortalWrt script
-#================================================= 
-
-
-##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/sony850218/ipk" >> feeds.conf.default
